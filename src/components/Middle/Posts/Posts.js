0ed34231082dfa7a41e9df6c2d718f943b6a7f24
Navneet <navnet.v.maurya@gmail.com>
2022-05-30 16:06:12 +0530

    Posts Section of Middle part is implemented

--- src/components/Middle/Posts/Posts.js
+++ src/components/Middle/Posts/Posts.js
@@ -1,5 +1,35 @@
+import { FcLikePlaceholder } from "react-icons/fc";
+import { FaRegCommentDots } from "react-icons/fa";
+import { BiShare } from "react-icons/bi";
+import Photo from "../../../images/mountain.jpg";
+import "./Posts.css";
+
 const Posts = () => {
-  return <div>posts</div>;
+  return (
+    <>
+      <div className="Post">
+        <img src={Photo} />
+        <div className="LikeComponents">
+          <FcLikePlaceholder />
+          <FaRegCommentDots />
+          <BiShare />
+        </div>
+        <span>23000 likes</span>
+        <span> Harshu Happy birthday!!</span>
+      </div>
+
+      <div className="Post">
+        <img src={Photo} />
+        <div className="LikeComponents">
+          <FcLikePlaceholder />
+          <FaRegCommentDots />
+          <BiShare />
+        </div>
+        <span>23000 likes</span>
+        <span> Harshu Happy birthday!!</span>
+      </div>
+    </>
+  );
 };
 
 export default Posts;
